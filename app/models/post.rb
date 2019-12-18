db560ab1d5daaf09582de33d28322ef87e23bb3b
Riley Moore <rilemoore@gmail.com>
2019-12-18 23:27:56 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,5 +9,5 @@ end
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
-  
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
 end
